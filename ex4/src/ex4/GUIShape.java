d44213f58c3c6b10dd9b9b0d46994d78fa5df2f0
97254 <adihoftman8@gmail.com>
2021-01-04 16:26:16 +0200

    finish shape

--- ex4/src/ex4/GUIShape.java
+++ ex4/src/ex4/GUIShape.java
@@ -5,54 +5,141 @@ package ex4;
  * @author I2CS
  */
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 import ex4.geometry.*;
 
 public class GUIShape implements GUI_Shape{
+	private GeoShape geoShape;
+	private int tag;
+	private boolean filled;
+	private Color color;
 
+	/**
+	 * Constructor that get empty all the variable.
+	 */
+	public GUIShape(){
+		this.geoShape = null;
+		this.filled = false;
+		this. color = new Color(000,000, 000);
+		this.tag = 0;
+	}
+
+	public GUIShape(GeoShape geo){
+		this.geoShape = geo;
+		this.filled = false;
+		this. color = new Color(000,000, 000);
+		this.tag = 0;
+	}
+
+	/**
+	 * Main constructor.
+	 * @param geo
+	 * @param f
+	 * @param c
+	 * @param t
+	 */
+	public GUIShape(GeoShape geo,boolean f ,Color c, int t){
+		this.geoShape = geo;
+		this.filled = f;
+		this.color = c;
+		this.tag = t;
+	}
+
+	/**
+	 * @return the shape of GUIShape.
+	 */
 	@Override
 	public GeoShape getShape() {
-		return null;
+		return this.geoShape;
 	}
 
+	/**
+	 * Change the shape of GUIShape.
+	 * @param g
+	 */
 	@Override
-	public void setShape(GeoShape g) {;
+	public void setShape(GeoShape g) {
+		this.geoShape = g;
 	}
 
+	/**
+	 * @return true if the shape is filled.
+	 */
 	@Override
 	public boolean isFilled() {
-		return false;	}
+		return this.filled;
+	}
 
+	/**
+	 * Change true or false if the shape is filled or not.
+ 	 * @param filled
+	 */
 	@Override
 	public void setFilled(boolean filled) {
-		;
+		this.filled = filled;
 	}
 
+	/**
+	 * @return the color of the shape.
+	 */
 	@Override
 	public Color getColor() {
-		return null;
+		return this.color;
 	}
 
+	/**
+	 * Change the color of the shape.
+	 * @param cl
+	 */
 	@Override
 	public void setColor(Color cl) {
-		;
+		this.color = cl;
 	}
 
+	/**
+	 * @return the tag of the shape.
+	 */
 	@Override
 	public int getTag() {
-		return -1;
+		return this.tag;
 	}
 
+	/**
+	 * Change the tag of the shape.
+	 * @param tag
+	 */
 	@Override
 	public void setTag(int tag) {
-		;
+		this.tag = tag;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	@Override
 	public GUI_Shape copy() {
-		return null;
+		return new GUIShape(this.geoShape, this.filled, this.color, this.tag);
 	}
 	@Override
 	public String toString() {
-		return null;
+		return "GUIShape," + this.geoShape
+				+ "," + this.filled
+				+ "," + this.color.getRGB()
+				+ "," + this.tag;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		GUIShape guiShape = (GUIShape) o;
+		return tag == guiShape.tag &&
+				filled == guiShape.filled &&
+				Objects.equals(geoShape, guiShape.geoShape) &&
+				Objects.equals(color, guiShape.color);
 	}
 }
--- ex4/src/ex4/Shape_Collection.java
+++ ex4/src/ex4/Shape_Collection.java
@@ -1,76 +1,146 @@
 package ex4;
 
-import java.io.File;
-import java.io.FileWriter;
+import java.awt.*;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Scanner;
+import java.awt.geom.Rectangle2D;
 
-import ex4.geometry.Circle2D;
-import ex4.geometry.GeoShape;
-import ex4.geometry.Point2D;
-import ex4.geometry.Rect2D;
+import ex4.geometry.*;
+
 /**
  * This class represents a collection of GUI_Shape.
  * Ex4: you should implement this class!
- * @author I2CS
  *
+ * @author I2CS
  */
-public class Shape_Collection implements GUI_Shape_Collection{
-	
-	@Override
-	public GUI_Shape get(int i) {
-		return null;
-	}
+public class Shape_Collection implements GUI_Shape_Collection, Comparator<GUI_Shape> {
+    private ArrayList<GUI_Shape> gui_shape;
+    private double x, y;
 
-	@Override
-	public int size() {
-		return -1;
-	}
+    public Shape_Collection() {
+        this.gui_shape = new ArrayList<>();
+    }
 
-	@Override
-	public GUI_Shape removeElementAt(int i) {
-		return null;
-	}
+    public Shape_Collection(Shape_Collection s) {
+        this.gui_shape = new ArrayList<GUI_Shape>(s.getArrayList());
+    }
 
-	@Override
-	public void addAt(GUI_Shape s, int i) {
-		;
-	}
-	@Override
-	public void add(GUI_Shape s) {
-		;
-	}
-	@Override
-	public GUI_Shape_Collection copy() {
-		return null;
-	}
+    public ArrayList<GUI_Shape> getArrayList() {
+        return this.gui_shape;
+    }
 
-	@Override
-	public void sort(Comparator comp) {
-		;
-	}
+    @Override
+    public GUI_Shape get(int i) {
+        return this.gui_shape.get(i);
+    }
 
-	@Override
-	public void removeAll() {
-		;
-	}
+    @Override
+    public int size() {
+        if (!this.gui_shape.isEmpty())
+            return this.gui_shape.size();
+        return 0;
+    }
 
-	@Override
-	public void save(String file) {
-		 ;
-	}
+    @Override
+    public GUI_Shape removeElementAt(int i) {
+        return this.gui_shape.remove(i);
+    }
 
-	@Override
-	public void load(String file) {
-		;
-	}
-	@Override
-	public Rect2D getBoundingBox() {
-		return null;
-	}
-	@Override
-	public String toString() {
-		return null;
-	}
+    @Override
+    public void addAt(GUI_Shape s, int i) {
+        this.gui_shape.add(i, s);
+    }
+
+    @Override
+    public void add(GUI_Shape s) {
+        if (!this.gui_shape.contains(s))
+            this.gui_shape.add(s);
+    }
+
+    @Override
+    public GUI_Shape_Collection copy() {
+        return new Shape_Collection(this);
+    }
+
+    @Override
+    public void sort(Comparator comp) {
+        this.gui_shape.sort(comp);
+    }
+
+    @Override
+    public void removeAll() {
+        this.gui_shape = new ArrayList<>();
+    }
+
+    @Override
+    public void save(String file) {
+        try {
+            File file1 = new File(file);
+            String s = "";
+            for (int i = 0; i < this.gui_shape.size(); i++) {
+                s += this.gui_shape.get(i) + ", " + "\n";
+            }
+            FileWriter fileW = new FileWriter(file1);
+            fileW.write(s);
+            fileW.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void load(String file) {
+        try {
+            File file1 = new File(file);
+            Scanner load = new Scanner(file1);
+            while (load.hasNextLine()) {
+                String next = load.nextLine();
+                for (int i = 0; i < this.gui_shape.size(); i++) {
+                    next += this.gui_shape.get(i);
+                }
+            }
+            load.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Rect2D getBoundingBox() {
+        ArrayList<Point2D> p = new ArrayList<>();
+        for (int i = 0; i < this.gui_shape.size(); i++) {
+            for (Point2D point : this.gui_shape.get(i).getShape().getPoints())
+                p.add(point);
+        }
+        double lp = Double.MAX_VALUE;
+        double rp = Double.MIN_VALUE;
+        double bp = Double.MAX_VALUE;
+        double tp = Double.MIN_VALUE;
+        for (int i = 0; i < p.size(); i++) {
+            if (p.get(i).x() >= rp)
+                rp = p.get(i).x();
+            if (p.get(i).x() <= lp)
+                lp = p.get(i).x();
+            if (p.get(i).y() >= tp)
+                tp = p.get(i).y();
+            if (p.get(i).y() <= bp)
+                bp = p.get(i).y();
+        }
+        Point2D pointR = new Point2D(rp, tp);
+        Point2D pointL = new Point2D(lp, bp);
+        return new Rect2D(pointR, pointL);
+    }
+
+    @Override
+    public String toString() {
+        return "" + this.gui_shape;
+    }
+
+    @Override
+    public int compare(GUI_Shape o1, GUI_Shape o2) {
+        if (o1.getTag() > o2.getTag()) return -1;
+        return 1;
+    }
 }
